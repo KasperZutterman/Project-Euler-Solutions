112fe5d9bdd71969c96e570c6e501f5368574d70
Kasper Zutterman <kasperzutterman1999@gmail.com>
2019-02-06 19:03:09 +0100

    Added p001

--- src/project/euler/p001.java
+++ src/project/euler/p001.java
@@ -13,8 +13,8 @@ public final class p001 {
     
     public String run() {
         int sum = 0;
-        for (int i = 0; i < 1000; i += 3) {
-            if (i % 3 ==0 && i % 5 == 0) {
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 ==0 || i % 5 == 0) {
                 sum += i;
             }
         }
